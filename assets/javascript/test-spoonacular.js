d812b32c45a547dedb8396fa8b668d5d11f72c49
Wayne <wayne.c.griffiths@gmail.com>
2020-03-21 11:43:17 +0800

    added a button to search with and an <section> to dispaly results in

---
+++ assets/javascript/test-spoonacular.js
@@ -0,0 +1,30 @@
+
+function randomKey(){
+    const KenKey = "c969a48f9cee4515b05f7efc8cf78b6b";
+    const VasaviKey = "4c88548ce7104df58f4630127aabfa78";
+    const WayneKey = "bbb86315753e407180066c6762ddee79";
+    const keyArray = [KenKey,WayneKey,VasaviKey];
+    const randomKey = keyArray[Math.floor(Math.random()*length.keyArray)];
+    return randomKey;
+}
+
+function constructQuery(){
+    const endpoint = "https://api.spoonacular.com/recipes/search?"
+    const APIkey = randomKey();
+    const query = "query="+$('#search-query').val()
+    const url = endpoint+query+APIkey
+    return url;
+}
+
+function ajaxRequest(URL){
+    $.ajax({
+        url: URL,
+        method: "GET"
+      }).then(displayResults);
+  }
+
+
+  
+function displayResults(ajaxResponse){
+
+}
