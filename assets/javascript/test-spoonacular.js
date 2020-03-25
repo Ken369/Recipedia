d1f6472476c1a6d4172dc3c5be2f40386160b1ae
Wayne <wayne.c.griffiths@gmail.com>
2020-03-25 22:44:04 +0800

    removed superfulous code from test-spoonacular

--- assets/javascript/test-spoonacular.js
+++ assets/javascript/test-spoonacular.js
@@ -1,46 +1,4 @@
 
-//---------------functions-----------------
-function randomKey() {
-  const KenKey = "c969a48f9cee4515b05f7efc8cf78b6b";
-  const VasaviKey = "4c88548ce7104df58f4630127aabfa78";
-  const WayneKey = "bbb86315753e407180066c6762ddee79";
-  const keyArray = [KenKey, WayneKey, VasaviKey];
-
-  randomKeyIndex = Math.floor(Math.random() * keyArray.length);
-  const randomKey = keyArray[randomKeyIndex];
-
-  return randomKey;
-}
-
-
-function getQuery(){
-    searchQuery = $('#search-query').val();
-   
-
-    return searchQuery;
-}
-
-function constructEndpointQuery(){
-    const excludeFoods = "placeholder" //
-    const number = 10 // number of results to return
-    const endpoint = "https://api.spoonacular.com/recipes/search?"; //base endpoint
-    const userKey = randomKey(); //api key     
-    const APIkey = "&apiKey="+userKey; //
-    const query = "query="+getQuery();
-    const url = endpoint+query+APIkey
-    console.log(url);
-    return url;
-}
-
-function endpointConstructor() {}
-
-function ajaxRequest(URL) {
-  $.ajax({
-    url: URL,
-    method: "GET"
-  }).then(displayResults);
-}
-
 function displayResults(ajaxResponse) {
   var recipeArray = ajaxResponse.results;
   console.log(ajaxResponse.results);
