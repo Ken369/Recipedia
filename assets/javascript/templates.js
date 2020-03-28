6384d2c62754c166bba84a81cfdfb9e1e65ec2c8
Wayne <wayne.c.griffiths@gmail.com>
2020-03-28 19:04:58 +0800

    removed BeanStalk pluggin because of bugs

--- assets/javascript/templates.js
+++ assets/javascript/templates.js
@@ -1,19 +1,15 @@
-const card  = sprout("div>div>(div>img+div>(span>(h2+p)))");
-console.log(card.info);
-why = {
-    div:{text:null},
-    img:{text:null, attributes:{}},
-    span:{text:null},
-    h2:{text:null},
-    p:{text:null},
-}
-
-content = {
-    title:"This is a title",
-    content:"this is some content",
-    image:{src:"https://via.placeholder.com/150", alt:"placeholder image"}
-};
-card.selector = extractSelectors("div>div>(div>img+div>(span>(h2+p)))");
-// card.selectors = why
-card.addContent(content);
-card.water('#search-results');
+`<div class="col s12 m4">
+<div class="card light-grey darken-1">
+    <img src="assets/images/star.png" width="20px">
+    <aside id="favourites">
+    <div class="card light-grey darken-1">
+            <div class="row valign-wrapper">
+                <div class="col s2">
+                  <img src="https://via.placeholder.com/150" alt="" class="circle responsive-img"> <!-- notice the "circle" class -->
+                </div>
+                <div class="col s10">
+                  <span class="black-text">
+                    Chicken Parma 
+                  </span>
+                </div>                
+    </div>`
